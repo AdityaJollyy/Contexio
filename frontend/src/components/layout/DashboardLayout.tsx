88a0ed27768b3920fcb7e49e056ca70fcf78886f
Aditya <adityajolly42@gmail.com>
2026-04-03 14:42:56 +0530

    Remove unused useEffect import from DashboardLayout component

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Sidebar, type FilterType } from "./Sidebar";
 import { TopBar } from "./TopBar";
